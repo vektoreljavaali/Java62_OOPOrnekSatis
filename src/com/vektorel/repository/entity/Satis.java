106ec6d10d0e765956ec47ee62bb906fdbba37df
vektoreljavaali <vektoreljavaali@gmail.com>
2022-02-20 10:03:59 +0300

    ...

---
+++ src/com/vektorel/repository/entity/Satis.java
@@ -0,0 +1,15 @@
+package com.vektorel.repository.entity;
+
+import java.util.Date;
+
+public class Satis {
+    long id;
+    long musteriid;
+    long urunid;
+    Date satistarihi;
+    Date islemtarihi;
+    double birimfiyat;
+    int adet;
+    double toplamfiyat;
+
+}
